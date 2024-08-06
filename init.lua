d682e0728ffe485ce8fc85b9c16917cafa275d77
Jack McGillivray <jack.mcgillivray@gmail.com>
2024-08-05 22:38:30 -0500

    clean up

--- init.lua
+++ init.lua
@@ -45,8 +45,6 @@ vim.opt.scrolloff = 10
 require 'keys'
 require 'auto'
 
--- [[ Install `lazy.nvim` plugin manager ]]
---    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.uv.fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
